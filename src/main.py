ecdb089b0f9e0641afe111c877f5adca59f00d5a
Kia Hadizade <kiahadizade3077@gmail.com>
2025-12-20 23:01:44 +0330

    Implemented the Minimal Only Visualization Code Milestone

--- src/main.py
+++ src/main.py
@@ -1,5 +1,23 @@
+import cv2
+from utils.video_io import open_video
+from utils.visualization import show_frame
+
 def main():
-    print("Color Object Tracker Pipeline")
+    cap = open_video(0)  # webcam
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        show_frame("Color Object Tracker", frame)
+
+        if cv2.waitKey(1) & 0xFF == 27:  # ESC to quit
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    # print("Color Object Tracker Pipeline")
 
 if __name__ == "__main__":
     main()
