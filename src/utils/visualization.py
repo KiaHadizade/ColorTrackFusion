b2168f91b1cf9535f6725ad5717c71ae7261cd59
Kia Hadizade <kiahadizade3077@gmail.com>
2025-12-25 00:00:02 +0330

    Added draw_bbox Function To Draw Green Bounding Box Around Detected Object

--- src/utils/visualization.py
+++ src/utils/visualization.py
@@ -1,7 +1,8 @@
 import cv2
 
-def draw_bbox(frame, bbox, color=(0,255,0)):
-    pass
+def draw_bbox(frame, bbox, color=(0, 255, 0), thickness=2):
+    x, y, w, h = bbox
+    cv2.rectangle(frame, (x, y), (x + w, y + h), color, thickness)
 
 def show_frame(window_name, frame):
     cv2.imshow(window_name, frame)
