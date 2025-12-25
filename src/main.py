0a5961803f6e3bb9092144a3bb4f476de9aef1e0
Kia Hadizade <kiahadizade3077@gmail.com>
2025-12-25 19:33:57 +0330

    Main File To Run Detection

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import cv2
 from utils.video_io import open_video
-from utils.visualization import show_frame
+from utils.visualization import show_frame, draw_bbox
+from detection.hsv_detector import detect_hsv_object
+from detection.color_ranges import COLOR_RANGES
 
 def main():
     cap = open_video(0)  # webcam
@@ -10,14 +12,19 @@ def main():
         if not ret:
             break
 
-        show_frame("Color Object Tracker", frame)
+        # show_frame("Color Object Tracker", frame)
+        for _, color_range in COLOR_RANGES.items():
+            bbox = detect_hsv_object(frame, color_range)
+            if bbox is not None:
+                draw_bbox(frame, bbox)
+
+        cv2.imshow("HSV Detection", frame)
 
         if cv2.waitKey(1) & 0xFF == 27:  # ESC to quit
             break
 
     cap.release()
     cv2.destroyAllWindows()
-    # print("Color Object Tracker Pipeline")
 
 if __name__ == "__main__":
     main()
